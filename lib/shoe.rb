992d2e038b3da6954f7a51e8f87c39911161ac76
Alie Stone <helloaliestone@gmail.com>
2020-09-25 18:53:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+class Shoe
+def initialized(brand)
+  @brand = brand
+end
+  
+attr_accessor :color :size :material :condition
+attr_reader :brand
+
+def cobble
+  puts "the shoe has been repaired"
+end
+
+end 
